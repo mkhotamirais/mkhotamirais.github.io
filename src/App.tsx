6f29e963c5bf45a2399c1f308ea7b061555fd3fe
Mkhotamirais <mkhotamirais@gmail.com>
2024-10-04 13:51:38 +0700

    tambah dikit

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ export default function App() {
     <div className="min-h-screen w-full flex items-center justify-center">
       <article className="prose text-center">
         <h1>Hello world</h1>
-        <p>My Name is Tami</p>
+        <p>My Name is Tami, I am a frontend developer</p>
       </article>
     </div>
   );
