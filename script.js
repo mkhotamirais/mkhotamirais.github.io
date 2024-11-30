46382b8c4a24eac0c13d55652e3b0391b39c4234
mkhotamirais <mkhotamirais@gmail.com>
2024-12-01 01:32:25 +0700

    migrasi lagi

--- script.js
+++ script.js
@@ -1,5 +1,24 @@
-const html = document.querySelector(".html");
-const css = document.querySelector(".css");
-const js = document.querySelector(".js");
+document.addEventListener("alpine:init", () => {
+  Alpine.store("lists", {
+    html,
+    css,
+    js,
+  });
+});
 
-html.innerHTML = htmlList.map((item, i) => `<a href="${item.href}">${i + 1}. ${item.label}</a>`);
+const html = [
+  { href: "html/html-1.html", label: "html-1" },
+  { href: "html/html-2.html", label: "html-2" },
+].sort((a, b) => a.label.localeCompare(b.label));
+
+const css = [
+  { href: "css/css-marquee.html", label: "css-marquee" },
+  { href: "css/robot-animation.html", label: "robot-animation" },
+  { href: "css/simple-tips.html", label: "simple-tips" },
+].sort((a, b) => a.label.localeCompare(b.label));
+
+const js = [
+  { href: "#", label: "vue js" },
+  { href: "js/alpine-wpu.html", label: "alpine wpu" },
+  { href: "#", label: "three js" },
+].sort((a, b) => a.label.localeCompare(b.label));
