a4a3f267be46f86261ff7b7f391b852571becb55
mkhotamirais <mkhotamirais@gmail.com>
2025-01-09 16:32:43 +0700

    ubah index

--- components/accordion/acc2/script.js
+++
@@ -1,8 +0,0 @@
-const accordionItem = document.querySelectorAll(".accordion-item");
-
-accordionItem.forEach((item, i) => {
-  item.addEventListener("click", () => {
-    item.classList.toggle("active");
-    accordionItem.forEach((item, j) => i !== j && item.classList.remove("active"));
-  });
-});
